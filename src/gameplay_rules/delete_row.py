48259660f0de5ca62dc54ff9ebbe68ecfb5b070d
lucatorsello <luca.torsello47@thehutgroup.com>
2024-02-23 11:49:16 +0000

    delete_row

---
+++ src/gameplay_rules/delete_row.py
@@ -0,0 +1,19 @@
+def clear_rows(grid, locked):
+
+    inc = 0
+    for i in range(len(grid) - 1, -1, -1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
